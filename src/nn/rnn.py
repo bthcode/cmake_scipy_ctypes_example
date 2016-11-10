02b1c589d5d917d18166090eb8930af40d775051
Brian Hone <brian.hone@stresearch.com>
2016-11-09 21:01:21 -0500

    x

--- src/nn/rnn.py
+++ src/nn/rnn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+np.set_printoptions(precision=3, suppress=True)
 
 # compute sigmoid nonlinearity
 def sigmoid(x):
@@ -24,6 +25,7 @@ num_chars = 8 # take 8 chars at a time
 w_ihs     = []
 w_hos     = []
 w_hhs     = []
+hs        = [0] * num_chars
 
 # SETUP
 for i in range(num_chars):
@@ -34,6 +36,7 @@ for i in range(num_chars):
 
 # INPUT PREP
 data       = open('input.txt', 'r').read() # should be simple plain text file
+data       = list(data)
 chars      = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
@@ -41,22 +44,57 @@ char_to_ix = { ch:i for i,ch in enumerate(chars) } # assigns each char a number 
 ix_to_char = { i:ch for i,ch in enumerate(chars) } # reverse lookup
 
 
-import pdb; pdb.set_trace()
-
-'''
 #################################################################################
 # in -> w_ih -> h_in -> sigmoid -> h_out -> w_ho -> out_in -> sigmoid -> out_out
-#        |                                   |
-#       c_ih                                c_oh
+#                                    |
+# in -> w_ih -> h_in -> sigmoid -> h_out -> w_ho -> out_in -> sigmoid -> out_out
+#                                    |
+# in -> w_ih -> h_in -> sigmoid -> h_out -> w_ho -> out_in -> sigmoid -> out_out
+#
 #################################################################################
 
-np.set_printoptions(precision=3, suppress=True)
 # Training
-sse    = 10
-epoch = 0
+sse      = 10
+epoch    = 0
+char_idx = 0
+set_size = num_chars+1
+
 while sse > 0.00001:
     epoch += 1
     sse = 0
+
+    # ------- Get Current Character Set ------- #
+    # grab n+1, handling wrap around
+    end_idx = min(len(data), char_idx+set_size)
+    ins = data[char_idx:end_idx]
+    # wrap around
+    if len(ins) < set_size:
+        end_idx = set_size - len(ins)
+        ins.extend(data[:end_idx])
+    char_idx = end_idx
+    if char_idx >= len(data):
+        char_idx = 0
+
+
+    # ------- Forward Pass ------- #
+    ins  = [ char_to_ix[x] for x in ins ]
+    ins  = ins[:-1]
+    outs = ins[1:]
+
+    for i in range(num_chars):
+        x = np.array( [[ins[i]]] )
+        if i > 0:
+            h_in = np.dot(x,w_ihs[i]) + np.dot(hs[i-1], w_hhs[i-1])
+        else:
+            h_in = np.dot(x,w_ihs[i])
+        h_in = np.dot(x, w_ihs[i])
+        h_out = sigmoid(h_in)
+        hs[i] = h_out
+
+        out_in = np.dot(h_out, w_hos[i])
+        out_out = sigmoid(out_in)
+    
+'''
     for idx, x in enumerate(ins):
         x        = x.reshape((1,len(x))) # covert from array to matrix
         # -- forward propagation --
