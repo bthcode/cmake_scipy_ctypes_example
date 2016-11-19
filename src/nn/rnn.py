89042354dcd6ea44788a1ac622ed36ca5d57043c
Brian Hone <brian.hone@stresearch.com>
2016-11-19 17:41:54 -0500

    rnn that matches karpathy's

--- src/nn/rnn.py
+++ src/nn/rnn.py
@@ -4,209 +4,174 @@ import numpy as np
 import string
 np.set_printoptions(precision=3, suppress=True)
 
-def sigmoid(x):
-    '''compute sigmoid nonlinearity'''
-    output = (2./(1+np.exp(-x))) - 0.5
-    return output
-# end sigmoid
-
-def dsigmoid(x):
-    '''convert output of sigmoid function to its derivative'''
-    return 2*np.exp(-x)/(1+np.exp(-x))**2
-# end dsigmoid
-
-
 np.random.seed(1)
 
-# INPUT PREP
-data       = open('input.txt', 'r').read() # should be simple plain text file
-data       = list(data)
-chars      = list(set(data))
-data_size  = len(data)
-vocab_size = len(chars)
-char_to_ix = { ch:i for i,ch in enumerate(chars) } # assigns each char a number - could be done with ord
-ix_to_char = { i:ch for i,ch in enumerate(chars) } # reverse lookup
+class rnn:
+    def __init__(self, vocab_size, num_chars):
+        self.vocab_size = vocab_size
 
-# INITIAL WEIGHTS
-num_chars = 24 # take 8 chars at a time
-w_ihs     = []
-w_hos     = []
-w_hhs     = []
+        self.num_chars = num_chars
 
-# Each nn has an ih and an ho
-eta       = 0.1
-alpha     = 0.1
-ni        = vocab_size # take one char as input to an individual nn
-no        = vocab_size # each layer has one char as output
-nh        = 16
+        # Each nn has an ih and an ho
+        self.eta   = 0.1
+        self.ni    = self.vocab_size # take one char as input to an individual nn
+        self.no    = self.vocab_size # each layer has one char as output
+        self.nh    = 100
 
-# SETUP
-for i in range(num_chars):
-    w_ihs.append(np.random.random((ni,nh)))
-    w_hos.append(np.random.random((nh,no)))
-    w_hhs.append(np.random.random((nh,nh)))
-# end
+        self.w_ih  = np.random.randn(self.nh,self.ni)*0.01
+        self.w_hh  = np.random.randn(self.nh,self.nh)*0.01
+        self.w_ho  = np.random.randn(self.no,self.nh)*0.01
 
-print 'data has %d characters, %d unique.' % (data_size, vocab_size)
+        self.bh    = np.zeros((self.nh, 1)) # hidden bias
+        self.by    = np.zeros((self.ni, 1)) # output bias
 
-#################################################################################
-# in -> w_ih -> h_pre -> sigmoid -> h_post -> w_ho -> out_pre -> sigmoid -> out_post
-#                                    |
-#                                  w_hh
-#                                    |
-# in -> w_ih -> h_pre -> sigmoid -> h_post -> w_ho -> out_pre -> sigmoid -> out_post
-#                                    |
-#                                   w_hh
-#                                    |
-# in -> w_ih -> h_pre -> sigmoid -> h_post -> w_ho -> out_pre -> sigmoid -> out_post
-#
-#################################################################################
+        self.m_w_ih  = np.zeros_like(self.w_ih)
+        self.m_w_hh  = np.zeros_like(self.w_hh)
+        self.m_w_ho  = np.zeros_like(self.w_ho)
 
-# Training
-sse      = 10
-epoch    = 0
-char_idx = 0
-set_size = num_chars+1
+        self.m_bh   = np.zeros_like(self.bh)
+        self.m_by   = np.zeros_like(self.by)
 
-# Place to put our inputs and outputs
-invecs     = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-outvecs    = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-h_pre      = [ np.zeros((1,nh)) for i in range(num_chars) ]
-h_post     = [ np.zeros((1,nh)) for i in range(num_chars) ]
-out_pre    = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-out_post   = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-err_o_post = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-err_o_pre  = [ np.zeros((1,vocab_size)) for i in range(num_chars) ]
-err_h_post = [ np.zeros((1,nh)) for i in range(num_chars) ]
-err_h_pre  = [ np.zeros((1,nh)) for i in range(num_chars) ]
+        self.hprev = np.zeros((self.nh,1))
+        self.smooth_loss = -np.log(1.0/self.vocab_size)*self.num_chars
+    # end __init__       
 
-#sample_ix = sample(h_post[0], cs[0], 500, w_ihs[0], w_hhs[0], w_hos[0], vocab_size)
-def sample(h, seed_ix, n, Wxh, Whh, Why, vocab_size):
-  """ 
-  sample a sequence of integers from the model 
-  h is memory state, seed_ix is seed letter for first time step
-  """
-  x = np.zeros((1,vocab_size))
-  x[0,seed_ix] = 1
-  ixes = []
-  for t in xrange(n):
-    #h_pre[i] = np.dot(x,w_ihs[i]) + np.dot(h_post[i-1], w_hhs[i-1])
-    #print x.shape
-    #print Wxh.shape
-    #print h.shape
-    #print Whh.shape
-    h = np.tanh(np.dot(x, Wxh) + np.dot(h,Whh))
-    #out_pre[i] = np.dot(h_post[i], w_hos[i]) # no x nchars
-    y = np.dot(h,Why)
-    p = np.exp(y) / np.sum(np.exp(y))
-    ix = np.random.choice(range(vocab_size), p=p.ravel())
-    x = np.zeros((1,vocab_size))
-    x[0,ix] = 1
-    ixes.append(ix)
-  return ixes
-# end sample
+    def train(self, chars):
+        # ------- Forward Pass ------- #
+        inputs  = chars[:-1]
+        targets = chars[1:]
 
+        loss = 0
 
-while sse > 0.00001:
-    epoch += 1
-    sse = 0
+        hs = {}
+        xs = {}
+        ys = {}
+        ps = {}
 
-    # ------- Get Current Character Set ------- #
-    # grab n+1, handling wrap around
-    end_idx = min(len(data), char_idx+set_size)
-    ins = data[char_idx:end_idx]
-    # wrap around
-    if len(ins) < set_size:
-        end_idx = set_size - len(ins)
-        ins.extend(data[:end_idx])
-    char_idx = end_idx
-    if char_idx >= len(data):
-        char_idx = 0
+        hs[-1] = np.copy(self.hprev)
 
-    # ------- Forward Pass ------- #
-    cs = [ char_to_ix[x] for x in ins ]
-    for idx, c in enumerate(cs[:-1]):
-        invecs[idx].fill(0)
-        invecs[idx][0,c] = 1
-    for idx, c in enumerate(cs[1:]):
-        outvecs[idx].fill(0)
-        outvecs[idx][0,c] = 1
+        ## Forward Pass ##
+        for t in xrange(len(inputs)):
+            char_index = inputs[t]
+            out_index  = targets[t]
+            xs[t] = np.zeros((vocab_size,1))
+            xs[t][char_index] = 1
+            hs[t] = np.tanh(np.dot(self.w_ih,xs[t]) + np.dot(self.w_hh,hs[t-1])+self.bh) # hidden state
+            ys[t] = np.dot(self.w_ho, hs[t]) + self.by  # unnormalized log probabilities for next chars
+            ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
+            prob  = ps[t][out_index,0]
+            loss += -np.log(prob) # softmax (cross-entropy loss)
 
-    for i in range(num_chars):
-        x = invecs[i]         # 1 x vocab
-        y = outvecs[i]        # 1 x vocab
-        if i > 0:
-            h_pre[i] = np.dot(x,w_ihs[0]) + np.dot(h_post[i-1], w_hhs[0])
-        else:
-            h_pre[i] = np.dot(x,w_ihs[0])
-        h_post[i] = sigmoid(h_pre[i])         # nh
+        ## Back Propagation ##
 
-        out_pre[i] = np.dot(h_post[i], w_hos[0]) # no x nchars
-        out_post[i] = sigmoid(out_pre[i])                 # no x nchars
+        d_w_ih = np.zeros_like(self.w_ih)
+        d_w_hh = np.zeros_like(self.w_hh)
+        d_w_ho = np.zeros_like(self.w_ho)
 
-    # --------- Error ------------ #
-    for i in range(num_chars):
-        err_o_post[i]  = outvecs[i] - out_post[i]              # no 
-        err_o_pre[i] = dsigmoid(out_pre[i]) * err_o_post[i]      # no (.*)
+        d_bh = np.zeros_like(self.bh)
+        d_by = np.zeros_like(self.by)
+        d_hnext = np.zeros_like(hs[0])
 
-    for i in range(num_chars):
-        idx = num_chars - i - 1 # reverse iterator
-        if i == 0: # no net to the right
-            err_h_post[idx] = np.dot(err_o_pre[i], w_hos[0].T) # nh
-        else:      # net to the right
-            #layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) + layer_2_delta.dot(synapse_1.T)) * sigmoid_output_to_derivative(layer_1)
-            err_h_post[idx] = np.dot(err_o_pre[i], w_hos[0].T) + np.dot(err_h_pre[idx+1], w_hhs[0]) # nh
-        err_h_pre[idx] = dsigmoid(err_h_post[idx]) * err_h_post[idx] #nh (.*)
+        for t in reversed(xrange(len(inputs))):
+            dy = np.copy(ps[t])
+            dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
+            d_w_ho += np.dot(dy, hs[t].T)
+            d_by += dy
+            d_h = np.dot(self.w_ho.T, dy) + d_hnext # backprop into h
+            d_hraw = (1 - hs[t] * hs[t]) * d_h # backprop through tanh nonlinearity
+            d_bh += d_hraw
+            d_w_ih += np.dot(d_hraw, xs[t].T)
+            d_w_hh += np.dot(d_hraw, hs[t-1].T)
+            d_hnext = np.dot(self.w_hh.T, d_hraw)
+        for dparam in [d_w_ih, d_w_hh, d_w_ho, d_bh, d_by]:
+            np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
 
-        
-    # --------- Update ---------- #
-    for i in range(num_chars):
-        d_ho     = np.dot(h_post[i].T, err_o_pre[i])
-        w_hos[0] = w_hos[0] + eta * d_ho 
-        d_ih     = np.dot(invecs[i].T, err_h_pre[i])
-        w_ihs[0] = w_ihs[0] + eta * d_ih 
+        self.hprev = hs[len(inputs)-1]
 
-    # --------- SSE -------------- #
-    sse = 0
-    for i in range(num_chars):
-        sse = sse + np.dot(err_o_post[i], err_o_post[i].T)[0,0]
-    sse = sse / float(num_chars)
+        self.m_w_ih = self.m_w_ih + d_w_ih * d_w_ih
+        self.w_ih   = self.w_ih - self.eta * d_w_ih / np.sqrt(self.m_w_ih + 1e-8)
+        self.m_w_hh = self.m_w_hh + d_w_hh * d_w_hh
+        self.w_hh   = self.w_hh - self.eta * d_w_hh / np.sqrt(self.m_w_hh + 1e-8)
+        self.m_w_ho = self.m_w_ho + d_w_ho * d_w_ho
+        self.w_ho   = self.w_ho - self.eta * d_w_ho / np.sqrt(self.m_w_ho + 1e-8)
 
-    if epoch % 1000 == 0:
-        #def sample(h, seed_ix, n, Wxh, Whh, Why, vocab_size):
-        print "________{0}_________".format(sse)
-        sample_ix = sample(h_post[0], cs[0], 500, w_ihs[0], w_hhs[0], w_hos[0], vocab_size)
-        sample_ix = [ ix_to_char[ix] for ix in sample_ix ]
-        print string.join(sample_ix, '')
-'''
-        x        = x.reshape((1,len(x))) # covert from array to matrix
-        # -- forward propagation --
-        h_in     = np.dot(x, w_ih)
-        h_out    = sigmoid(h_in)
-        out_pre   = np.dot(h_out, w_ho)
-        out_post  = sigmoid(out_pre) 
+        self.m_bh   = self.m_bh + d_bh * d_bh
+        self.bh     = self.bh   - self.eta * d_bh / np.sqrt(self.m_bh + 1e-8)
+        self.m_by   = self.m_by + d_by * d_by
+        self.by     = self.by   - self.eta * d_by / np.sqrt(self.m_by + 1e-8)
+
+        return loss
+    # end train
+
+    def sample(self, nchars, seed_ix):
+        """ 
+        sample a sequence of integers from the model 
+        h is memory state, seed_ix is seed letter for first time step
+        """
+        x = np.zeros((self.vocab_size,1))
+        x[seed_ix] = 1
+        ixes = []
+        h = self.hprev.copy()
+        for t in range(nchars):
+            h = np.tanh(np.dot(self.w_ih,x) + np.dot(self.w_hh,h)) # hidden state
+            y = np.dot(self.w_ho, h)
+            p = np.exp(y) / np.sum(np.exp(y))
+            ix = np.random.choice(range(self.vocab_size), p=p.ravel())
+            x = np.zeros((self.vocab_size,1))
+            x[ix] = 1
+            ixes.append(ix)
+        return ixes
+    # end sample
 
 
-        # -- error calculation --
-        err_o    = expected[idx] - out_post
-        err_op   = dsigmoid(out_pre) * err_o
-        err_h    = np.dot(err_op, w_ho.T)
-        err_hp   = dsigmoid(h_in) * err_h
-            
-        # -- weights adjustment --
-        d_ho     = np.dot(h_out.T, err_op)
-        w_ho     = w_ho + eta * d_ho + alpha * c_ho
-        c_ho     = d_ho.copy() # Store momentum
-        
-        d_ih     = np.dot(x.T, err_hp)
-        w_ih     = w_ih + eta * d_ih + alpha * c_ih
-        c_ih     = d_ih.copy()
+# end class rnn
 
-        # -- sum of squares error
-        sse = sse + np.dot(err_o, err_o.T)[0,0]
-        pred[idx,:] = out_post
-    if epoch % 100 == 0:
-        print epoch, sse
-print "After {0} epochs:".format(epoch)
-print pred
-'''
+if __name__=="__main__":
+    # INPUT PREP
+    data       = open('input.txt', 'r').read() # should be simple plain text file
+    data       = list(data)
+    chars      = list(set(data))
+    data_size  = len(data)
+    vocab_size = len(chars)
+    char_to_ix = { ch:i for i,ch in enumerate(chars) } # assigns each char a number - could be done with ord
+    ix_to_char = { i:ch for i,ch in enumerate(chars) } # reverse lookup
+    num_chars  = 25
+
+    R          = rnn(vocab_size, num_chars)
+
+    # Training
+    epoch    = 0
+    char_idx = 0
+    set_size = num_chars+1
+
+    loss = 10000
+    smooth_loss = -np.log(1.0/vocab_size)*num_chars
+    while loss > 0.00001:
+        epoch += 1
+        loss = 0
+
+        # ------- Get Current Character Set ------- #
+        # grab n+1, handling wrap around
+        end_idx = min(len(data), char_idx+set_size)
+        ins = data[char_idx:end_idx]
+
+        # wrap around
+        if len(ins) < set_size:
+            end_idx = set_size - len(ins)
+            ins.extend(data[:end_idx])
+        char_idx = end_idx-1
+        if char_idx >= len(data):
+            char_idx = 0
+            R.hprev = np.zeros((R.nh,1)) # reset RNN memory
+
+        cs = [ char_to_ix[x] for x in ins ]
+        loss = R.train(cs)
+        smooth_loss = smooth_loss * 0.999 + loss * 0.001
+
+        if epoch % 100 == 0:
+            print "-------------- Epoch : {0}, loss = {1} ".format(epoch, smooth_loss)
+        if epoch % 1000 == 0:
+            print ins
+            sample_idxs = R.sample(500, cs[0])
+            sample_chars = [ ix_to_char[ix] for ix in sample_idxs ]
+            print string.join(sample_chars, '')
